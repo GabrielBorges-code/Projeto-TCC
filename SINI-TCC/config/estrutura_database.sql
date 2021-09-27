e7403462a3d0ef01980242176fd0a0d0dff51f8d
Gabriel Borges <gabriel.moura2009@hotmail.com>
2021-09-27 19:51:16 -0300

    ajuste no banco de dados

--- SINI-TCC/config/estrutura_database.sql
+++ SINI-TCC/config/estrutura_database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE sistema_investimento_tcc;
 USE sistema_investimento_tcc;
 
 CREATE TABLE IF NOT EXISTS usuario (
-    id int (11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome varchar (100),
     email varchar (100),
     telefone varchar (50),
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS usuario (
 CREATE TABLE IF NOT EXISTS mensagem_contato (
 	id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar (100),
+    telefone varchar (50),
     nome varchar (100),
     mensagem TEXT
 );
