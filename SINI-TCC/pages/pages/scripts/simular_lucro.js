820406354429863c3ab588cba89496a817e3e77b
Gabriel Borges <gabriel.moura2009@hotmail.com>
2021-11-13 20:53:22 -0300

    funcionalidade para simular lucros

--- SINI-TCC/pages/pages/scripts/simular_lucro.js
+++ SINI-TCC/pages/pages/scripts/simular_lucro.js
@@ -1,14 +1,22 @@
 var dinheiroInvestido = document.querySelector("div.dinheiro-investido");
+var prazoMeses = document.querySelector("div.prazo-meses");
 var jurosGanhos = document.querySelector("div.juros-ganhos");
 var totalGanhos = document.querySelector("div.total-ganhos");
+var imposto = document.querySelector("div.imposto-sobre-ganhos");
 
 function calcular () {
-    window.alert("Você ganhou muito dinheiro");
+    var inputValorInvestido = document.querySelector("input#valorInvestir");
+    var inputPrazo = document.querySelector("input#prazo");
+    var inputRentabilidade = document.querySelector("input#rentabilidade");
 
-    dinheiroInvestido.innerHTML = "Você investiu R$ X";
-    jurosGanhos.innerHTML = "Você está ganhando R$ y";
-    totalGanhos.innerHTML = "Totalizando R$ z";
+    var lucro = inputValorInvestido.value * (inputRentabilidade.value / 100 * inputPrazo.value);
 
+    var impostoSobreGanhos = lucro - (lucro * 0.175);
 
+    dinheiroInvestido.innerHTML = "Você investiu R$ " + inputValorInvestido.value;
+    prazoMeses.innerHTML = "Por " + inputPrazo.value + " Meses";
+    jurosGanhos.innerHTML = "Com rentabilidade de " + inputRentabilidade.value + "%";
+    totalGanhos.innerHTML = "Totalizando R$ " + lucro.toFixed(2);
+    imposto.innerHTML = "Imposto sobre ganhos de 17.5%, R$ " + impostoSobreGanhos.toFixed(2);
 
 }
